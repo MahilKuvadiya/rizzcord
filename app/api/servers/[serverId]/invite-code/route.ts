150f4354cdce286c16b50c98027064d0fdc22dc5
Mahil <mahilpatel6858@gmail.com>
2024-03-30 16:11:57 +0530

    lil bug : 3

--- app/api/servers/[serverId]/invite-code/route.ts
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -22,7 +22,6 @@ export async function PATCH(
     const server = await db.server.update({
       where: {
         id: params.serverId,
-        profileId: profile.id,
       },
       data: {
         inviteCode: uuidv4(),
